44eaf6b60e879f8c99d7db80d44fece96146b7c3
Apoorv Khurasia <apoorvkhurasia@users.noreply.github.com>
2024-02-21 10:15:14 +0000

    Working rendering

--- main.js
+++ main.js
@@ -3,282 +3,149 @@ import * as cmp from "./lib/components.js";
 
 const State = {
     TOPICS: "topics",
-    TABLE_OF_CONTENTS: "table-of-contents",
-    CHAPTER_DISPLAY_BUT_NOT_EDIT: "chapter-no-edit",
-    CHAPTER_EDIT: "chapter-edit",
-    CHAPTER_EDIT_FINISHED: "chapter-edit-finished",
-    CHAPTER_EDIT_IN_PROGRESS: "chapter-edit-in-progress"
+    CHAPTER_LIST: "chapters",
 }
 
+var currentState = null;
 var currentContentRoot = null;
 var currentTopic = null;
 var currentChapter = null;
 
 $(function () {
-    initApp();
-    // const increaseFontSizeBtn = document.getElementById("increase-font-size");
-    // increaseFontSizeBtn.addEventListener("click", increaseInputFontSize);
-
-    // const decreaseFontSizeBtn = document.getElementById("decrease-font-size");
-    // decreaseFontSizeBtn.addEventListener("click", decreaseInputFontSize);
-});
-
-function initApp() {
-    state(State.TOPICS);
+    setState(State.TOPICS);
     const openFileLink = document.getElementById("open-store");
     openFileLink.addEventListener("click", async () => {
         currentContentRoot = await window.showDirectoryPicker();
-        await createTopicList(dirHandle);
+        setState(State.TOPICS);
+        await createTopicList(currentContentRoot);
     });
 
     const markdownInputArea = document.getElementById("markdownInput");
-    markdownInputArea.addEventListener("onkeyup", render);
+    markdownInputArea.addEventListener("input", render);
+
+    const newItemLink = document.getElementById("new-topic-or-chapter");
+    newItemLink.addEventListener("click", getTopicOrChapterNameFromUser);
+
+    const inputForm = document.getElementById("topic-or-chapter-input");
+    inputForm.addEventListener("onkeyup", async (e) => {
+        if (inputForm.is(":focus") && e.key === 'Enter') {
+            await createNewTopicOrChapter(inputForm.val());
+            inputForm.hide();
+        }
+    });
+});
+
+function getTopicOrChapterNameFromUser() {
+    if (!currentContentRoot) return;
+    const inputForm = document.getElementById("topic-or-chapter-input");
+    inputForm.show();
 }
 
-async function createTopicList(contentRootHandle) {
+async function createNewTopicOrChapter(userInput) {
+    if (!currentContentRoot) return;
+
+    const contentRootHandle = currentContentRoot;
+    if (!userInput) {
+        return;
+    }
+
+    const explorerListElem = $("#explorer-items");
+    if (currentState === State.TOPICS) {
+        topic = await fs.createNewTopic(contentRootHandle, userInput);
+        explorerListElem.appendChild(cmp.createListItem(
+            topic.displayName,
+            async () => displayTopic(topic)));
+    } else if (currentState === State.CHAPTER_LIST) {
+        chapter = await fs.createNewChapter(contentRootHandle, chapterName);
+        explorerListElem.appendChild(cmp.createListItem(
+            chapter.displayName,
+            async () => displayChapter(chapter)));
+    }
+}
+
+async function createTopicList() {
+    if (!currentContentRoot || currentState !== State.TOPICS) return;
+
+    const contentRootHandle = currentContentRoot;
     const topicListElem = document.getElementById("explorer-items");
+    while (topicListElem.firstChild) {
+        topicListElem.removeChild(topicListElem.firstChild);
+    }
     const topics = await fs.getTopics(contentRootHandle);
+    topics.sort();
     for (const topic of topics) {
         topicListElem.appendChild(cmp.createListItem(
             topic.displayName,
-            async () => displayTopic(contentRootHandle, topic)));
+            async () => displayTopic(topic)));
     }
-
-    const topicListLink = document.getElementById("toplist-link");
-    topicListLink.removeEventListener("click");
-    topicListLink.addEventListener("click", async () => createTopicList(contentRootHandle));
-
-    const newTopicCreateListItem = document.createElement("li");
-    const newTopicCreationHintLabel = document.createElement("label");
-    newTopicCreationHintLabel.appendChild(document.createTextNode("Create a new topic with name: "));
-    newTopicCreateListItem.appendChild(newTopicCreationHintLabel);
-
-    const newTopicNameInput = document.createElement("input");
-    newTopicNameInput.setAttribute("id", "newTopicName");
-    newTopicNameInput.setAttribute("name", "newTopicName");
-    newTopicCreateListItem.appendChild(newTopicNameInput);
-
-    const createNewTopicBtn = document.createElement("button");
-    createNewTopicBtn.setAttribute("id", "createNewTopicBtn");
-    createNewTopicBtn.setAttribute("name", "createNewTopicBtn");
-    createNewTopicBtn.appendChild(document.createTextNode("Create"));
-    createNewTopicBtn.addEventListener("click", async () => {
-        createNewTopicBtn.setAttribute("disabled", true); //disabled until callback completes
-        const topicName = newTopicNameInput.value;
-        try {
-            const createdTopic = await fs.createNewTopic(contentRootHandle, topicName);
-            await displayTopic(createdTopic);
-        } catch (err) {
-            console.log(err);
-        } finally {
-            createNewTopicBtn.setAttribute("disabled", false);
-        }
-    });
-
-    newTopicCreateListItem.appendChild(createNewTopicBtn);
-    topicListElem.appendChild(newTopicCreateListItem);
-    appendAsOnlyChild("#topics", topicListElem);
-    state(State.TOPICS);
 }
 
-async function displayTopic(contentRootHandle, topic, edit = false) {
+async function displayTopic(topic) {
+    if (!currentContentRoot) return;
+    setState(State.CHAPTER_LIST);
     clearChapterContent();
     currentTopic = topic;
     currentChapter = null;
-
-    const chapterList = await fs.getChapters(contentRootHandle, topic);
-    updateTableOfContents(contentRootHandle, topic, chapterList);
-    state(State.TABLE_OF_CONTENTS);
-    if (edit) {
-        appendChapter();
+    if (topic) {
+        const chapterList = await fs.getChapters(currentContentRoot, topic);
+        chapterList.sort();
+        updateTableOfContents(chapterList);
     }
 }
 
-async function displayChapter(contentRootHandle, chapter, edit = false, updateTOC = false) {
+async function displayChapter(chapter) {
+    if (!currentContentRoot) return;
     currentChapter = chapter;
     if (chapter) {
         currentTopic = chapter.topic;
-        $("#chapterName").val(currentChapter.displayName);
-        const rawText = await fs.getChapterRawText(contentRootHandle, chapter);
+        const rawText = await fs.getChapterRawText(currentContentRoot, chapter);
         $("#markdownInput").val(rawText);
         render();
-        if (!edit) {
-            state(State.CHAPTER_DISPLAY_BUT_NOT_EDIT);
-        } else {
-            state(State.CHAPTER_EDIT);
-        }
-        if (updateTOC) {
-            updateTableOfContents(chapter.topic, []);
-        }
     }
 }
 
-function appendChapter() {
-    currentChapter = null;
-    clearChapterContent();
-    state(State.CHAPTER_EDIT);
-}
+function updateTableOfContents(chapterList) {
+    if (currentState !== State.CHAPTER_LIST) return;
+    if (!currentContentRoot) return;
 
-function updateTableOfContents(contentRootHandle, topic, chapterList) {
-    appendAsOnlyChild("#heading", document.createTextNode("Notes on " + topic.displayName));
-    const chapterListElems = document.createElement("ol");
+    const chapterListElems = document.getElementById("explorer-items");
+    while (chapterListElems.firstChild) {
+        chapterListElems.removeChild(chapterListElems.firstChild);
+    }
+
     for (const chapterToDisplay of chapterList) {
         chapterListElems.appendChild(
-            createListItem(
+            cmp.createListItem(
                 chapterToDisplay.chapterName,
-                async () => await displayChapter(contentRootHandle, chapterToDisplay)
+                async () => await displayChapter(chapterToDisplay)
             )
         );
     }
-
-    const appendNewChapterElem = createListItem("Append", () => appendChapter());
-    appendNewChapterElem.appendChild(document.createTextNode(" a new chapter on this topic."));
-    chapterListElems.appendChild(appendNewChapterElem);
-    appendAsOnlyChild("#chapters", chapterListElems);
-}
-
-async function upsertChapter(contentRootHandle) {
-    var chapterToSave = currentChapter;
-    if (!chapterToSave) {
-        chapterToSave = new Chapter(currentTopic, chapterName.value);
-    }
-    state(State.CHAPTER_EDIT_IN_PROGRESS);
-    try {
-        chapter = await fs.saveChapter(contentRootHandle, currentChapter, markdownInput.value);
-        displayChapter(contentRootHandle, chapter, false, true);
-    } catch (err) {
-        console.log(err);
-        state(State.ERROR, err);
-    } finally {
-        state(State.CHAPTER_EDIT_FINISHED);
-    }
-}
-
-async function cancelEditChapter(contentRootHandle) {
-    //Whenever we next go into edit mode; we will enable these buttons
-    $("#finishEdit").prop("disabled", true);
-    $("#cancelEdit").prop("disabled", true);
-    if (currentChapter) {
-        await displayChapter(contentRootHandle, currentChapter);
-    } else if (currentTopic) {
-        await displayTopic(currentTopic);
-    } else {
-        await createTopicList(contentRootHandle);
-    }
-}
-
-function appendAsOnlyChild(selector, childElem) {
-    $(selector).empty();
-    $(selector).append(childElem);
-}
-
-function showEditor() {
-    $("#editor").show();
-    $("#editLink").hide();
-}
-
-function hideEditor() {
-    $("#editor").hide();
-    $("#editLink").show();
 }
 
 function render() {
-    var underScoreEscapedMarkdown = $("#markdownInput").val().replaceAll("_", "\\_");
-    var parsedHTML = marked.parse(underScoreEscapedMarkdown);
-    $("#parsedMarkdown").html(parsedHTML);
-    Preview.Update();
+    var md = $("#markdownInput").val();
+    var parsedHTML = marked.parse(md);
+    $("#preview").html(parsedHTML);
 }
 
-function increaseInputFontSize() {
-    var currFontSize = parseInt($("#markdownInput").css("font-size").replace("px", ""));
-    $("#markdownInput").css("font-size", (currFontSize + 1) + "px");
-}
-
-function decreaseInputFontSize() {
-    var currFontSize = parseInt($("#markdownInput").css("font-size").replace("px", ""));
-    $("#markdownInput").css("font-size", (currFontSize - 1) + "px");
-}
-
-function cancelEditChapter() {
-    //Whenever we next go into edit mode; we will enable these buttons
-    $("#finishEdit").prop("disabled", true);
-    $("#cancelEdit").prop("disabled", true);
-    if (currentChapter) {
-        displayChapter(currentChapter);
-    } else if (currentTopic) {
-        displayTopic(currentTopic);
-    } else {
-        createTopicList();
-    }
-}
-
-function showErrorDetails() {
-    $("#errorDetails").show();
-    $("#showHideErrLink").text("Hide Technical Details");
-    errorDetailsVisible = true;
-}
-
-function hideErrorDetails() {
-    $("#errorDetails").hide();
-    $("#showHideErrLink").text("Show Technical Details");
-    errorDetailsVisible = false;
-}
-
-function toggleErrorMsgDetails() {
-    if (errorDetailsVisible === false) {
-        showErrorDetails();
-    } else {
-        hideErrorDetails();
-    }
-}
-
-function state(state, errorTechDetails) {
-    if (typeof (errorTechDetails) === "undefined") errorTechDetails = "No technical details available.";
+function setState(state) {
+    currentState = state;
     switch (state) {
         case State.TOPICS:
-            $("#allTopics").show();
-            $("#topicContent").hide();
-            $("#error").hide();
+            $("#preview").hide();
+            $("#main-content").hide();
+            $("#save-chapter").hide();
+            $("#discard-chapter").hide();
+            $("#new-topic-or-chapter").textContent = "New Topic";
             break;
-        case State.TABLE_OF_CONTENTS:
-            $("#chapterContent").hide();
-            $("#allTopics").hide();
-            $("#topicContent").show();
-            $("#error").hide();
-            break;
-        case State.CHAPTER_DISPLAY_BUT_NOT_EDIT:
-            $("#allTopics").hide();
-            $("#chapterContent").show();
-            $("#topicContent").show();
-            $("#error").hide();
-            hideEditor();
-            break;
-        case State.CHAPTER_EDIT:
-            $("#allTopics").hide();
-            $("#chapterContent").show();
-            $("#topicContent").show();
-            $("#finishEdit").prop("disabled", false);
-            $("#cancelEdit").prop("disabled", false);
-            $("#error").hide();
-            showEditor();
-            break;
-        case State.CHAPTER_EDIT_FINISHED:
-            $("#finishEdit").prop("disabled", false);
-            $("#cancelEdit").prop("disabled", false);
-            break;
-        case State.CHAPTER_EDIT_IN_PROGRESS:
-            $("#finishEdit").prop("disabled", true);
-            $("#cancelEdit").prop("disabled", true);
-            break;
-        case State.ERROR:
-            $("#allTopics").hide();
-            $("#editLink").hide();
-            $("#editor").hide();
-            $("#chapterContent").hide();
-            $("#topicContent").hide();
-            $("#errorDetails").html(errorTechDetails);
-            hideErrorDetails();
-            $("#error").show();
+        case State.CHAPTER_LIST:
+            $("#main-content").show();
+            $("#preview").show();
+            $("#markdownInput").show();
+            $("#save-chapter").show();
+            $("#discard-chapter").show();
+            $("#new-topic-or-chapter").textContent = "New Chapter";
             break;
         default:
             console.error("Invalid state: " + state);
@@ -287,7 +154,6 @@ function state(state, errorTechDetails) {
 }
 
 function clearChapterContent() {
-    chapterName.value = "";
     $("#markdownInput").val("");
-    render();
+    $("#preview").val("");
 }
